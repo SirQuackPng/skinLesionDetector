04df59f4351499c01d43d259e4b117b28b7a85c2
SirQuackPng <195597714+SirQuackPng@users.noreply.github.com>
2025-06-02 15:45:36 +0100

    started training

--- main.py
+++ main.py
@@ -6,28 +6,30 @@ import numpy as np
 import time
 
 # creates a neural network class
-NN = Network(lossFunc = "cross", learningRate = 0.001)
-NN.addLayer(32, "relu")
-NN.addLayer(7, "softmax")
+NN = Network(lossFunc = "cross", learningRate = 0.00025)
+NN.addLayer(128, "relu")
+NN.addLayer(3, "softmax")
 
-NN.createWeightsAndBiases()
+#depths = [3, 32, 64]
+#numKernals = [32, 64, 128] 
 
-depths = [3, 32, 32, 32]
+depths = [3, 64]
+numKernals = [64, 128] 
 
 # creates a convulational neural network class
 cnn = quacknet.convulationalManager.CNNModel(NN)
-for i in range(4): 
+for i in range(2): 
     cnn.addLayer(ConvLayer(
         kernalSize = 3,
         depth = depths[i],
-        numKernals = 32,
-        stride = 2, 
+        numKernals = numKernals[i],
+        stride = 1, 
         padding = "n"
     ))
     cnn.addLayer(ActivationLayer())
     cnn.addLayer(PoolingLayer(
         gridSize = 2,
-        stride = 1,
+        stride = 2,
         mode = "max",
     ))
 
@@ -40,19 +42,42 @@ cnn.addLayer(DenseLayer(
     NN
 ))
 
+
 cnn.createWeightsBiases()
+NN.createWeightsAndBiases()
+cnn.saveModel(NN.weights, NN.biases, cnn.weights, cnn.biases)
 
-images = np.load("processedData\ham10000_images.npy", mmap_mode='r')
-labels = np.load("processedData\ham10000_labels.npy", mmap_mode='r')
+cnn.loadModel(NN)
 
-print("started")
+images = np.load("processedData/training/ham10000_images.npy", mmap_mode='r')
+labels = np.load("processedData/training/ham10000_labels.npy", mmap_mode='r')
+
+# randomly shuffles images and labels
+indices = np.random.permutation(len(images))
+images = images[indices]
+labels = labels[indices]
+
+batchSize = 64
+print("started") 
 start = time.time()
 accauracy, loss = cnn.train(
-    inputData=images[:32],
-    labels=labels[:32],
+    inputData=images,
+    labels=labels,
     useBatches=True,
-    batchSize=32
+    batchSize=batchSize,
+    alpha=0.00025,
 )
+print(f"took: {(time.time() - start)} seconds or {(time.time() - start) / 60} minutes")
+print(f"accauracy: {accauracy}")
+print(f"loss: {loss}")
 
-print(f"took: {time.time() - start} seconds")
-print(accauracy, loss)
+
+cnn.saveModel(NN.weights, NN.biases, cnn.weights, cnn.biases)
+
+'''
+accauracy:
+
+
+loss:
+
+'''
--- processData.py
+++ processData.py
@@ -1,37 +1,81 @@
+'''
+Basically in the HAM10000 dataset their is a very large class imbalance:
+nv, 6705
+mel, 1113
+bkl, 1099
+bcc, 514
+akiec, 327
+vasc, 142
+df, 115
+
+and so basically the 3 largest classes takes up 89% of all the images
+so instead the model only trains on the 3 largest classes 
+and also makes each class the same size so all of them 1099 size * 3
+
+so the dataset goes from 10000 to 3297
+'''
 from quacknet.dataAugmentation import Augementation
 import pandas as pd
 import numpy as np
 import os
+import random
 
 augmentation = Augementation()
 imagePaths = augmentation.getImagePaths('rawData/data/')
+random.shuffle(imagePaths)
 
 metadata = pd.read_csv('rawData/HAM10000_metadata.csv')
 labelsDict = dict(zip(metadata['image_id'], metadata['dx']))
 
 labelIdDict = {
-    "nv": 0,
-    "mel": 1,
-    "bcc": 2,
+    "nv": 0,      #largest
+    "mel": 1,     #second largest
+    "bkl": 2,     #third largest
     "akiec": 3,
-    "bkl": 4,
+    "bcc": 4,
     "df": 5,
     "vasc": 6,
 }
 
+topClasses = ["nv", "mel", "bkl"]
+
 labels = []
+images = []
 for i in imagePaths:
     imageId = os.path.basename(i)[:-4] # removes the .jpg extsension
-    labels.append(labelIdDict[labelsDict[imageId]])
-labels = augmentation.hotEncodeLabels(labels, 7)
+    label = labelsDict[imageId]
+    if(label in topClasses):
+        labels.append(labelIdDict[label])
+        images.append(i)
+imagePaths = images
+labels = augmentation.hotEncodeLabels(labels, 3) #only 3 classes
 
-processedImage = augmentation.preprocessImages(imagePaths, (128, 128))
-images = augmentation.dataAugmentation(processedImage)
-images = images.transpose(0, 3, 1, 2)
+processedImage = augmentation.preprocessImages(imagePaths, (64, 64))
+images = processedImage.transpose(0, 3, 1, 2)
+labelsAugmented = labels 
 
-labelsAugmented = np.repeat(labels, 4, axis=0)
+count = np.zeros(len(labelsAugmented[0]))
+for i in range(len(labelsAugmented)):
+    count = count + np.array(labelsAugmented[i])
 
-np.save('processedData/ham10000_images.npy', np.array(images, dtype=np.float32))
-np.save('processedData/ham10000_labels.npy', np.array(labelsAugmented))
+sorted = np.argsort(count)
+largestClass = sorted[-1] #largest class index
+secondClass = sorted[-2] #second largest class index
+thirdClass = sorted[-3] #third largest class index
 
+thirdLargestClass = int(count[thirdClass]) #gets the size of the 3rd largest class
 
+allClasses = [[], [], []]
+for i in range(len(images)):
+    allClasses[np.argmax(labelsAugmented[i])].append((images[i], labelsAugmented[i]))
+
+allImages = []
+allLabels = []
+for i in range(len(allClasses)):
+    if(i == largestClass or i == secondClass or i == thirdClass):
+        samples = allClasses[i][:thirdLargestClass]
+        allImages.extend([sample[0] for sample in samples])
+        allLabels.extend([sample[1] for sample in samples])
+
+np.save('processedData/training/ham10000_images.npy', np.array(allImages, dtype=np.float32))
+np.save('processedData/training/ham10000_labels.npy', np.array(allLabels))
